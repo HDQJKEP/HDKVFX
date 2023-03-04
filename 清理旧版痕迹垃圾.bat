286aa03e0f46df6774c7cce47b4e629705455a7a
剑龙秋鹤 <71777983+HDQJKEP@users.noreply.github.com>
2023-03-05 01:53:11 +0800

    Add files via upload

---
+++ 清理旧版痕迹垃圾.bat
@@ -0,0 +1,88 @@
+@echo off
+
+rd /s /q  "C:\HDQJKEP_PSTPA\Houdini\HDQJKEP_packages\HDQJKEPLABS18.5"
+
+rd /s /q  "C:\HDQJKEP_PSTPA\Houdini\HDQJKEP_packages\HDQJKEPLABS19.0"
+
+rd /s /q  "C:\HDQJKEP_PSTPA\Houdini\HDQJKEP_packages\HDQJKEPLABS19.5"
+
+rd /s /q  "C:\HDQJKEP_PSTPA\Houdini_Packages"
+
+rd /s /q  "C:\HDQJKEP_Tool"
+
+
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*Light topography - Cut out excess.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*Screen_camera_control_switch.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*Type_split_output_by_lens.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*Animation_character_switch.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*Animation_area.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*AssetsHandling_BooleanToDig_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*AssetsHandling_CoordinateKeyingSuperfluous_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*AssetsHandling_ObjnameObtain_V2.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*Comprehensive_DisplayPackageModelColors_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*Comprehensive_MaskAutomaticSegmentation_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*Comprehensive_MaskInsideAndExtractOutput_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DataAssets_GetMayaMashData_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DataAssets_MayaMashDataAssetCopyReplacement_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DistributedWorkflow_AnimationCharacterSwitch_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DistributedWorkflow_AnimationPerformanceArea_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DistributedWorkflow_DisplaySwitchingControl_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DistributedWorkflow_ExportSplitAdjustmentData_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DistributedWorkflow_ImportSplitAdjustmentData_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DistributedWorkflow_MergeDataExport_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DistributedWorkflow_ObtainObjnameIDGroup_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DistributedWorkflow_PositionSegmentationAdjustment_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*DistributedWorkflow_SubstitutionAdjustmentOutput_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini18.5\otls\*PlantsSpread_MergeDataExport_V1.0N.hda*
+
+
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*Light topography - Cut out excess.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*Screen_camera_control_switch.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*Type_split_output_by_lens.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*Animation_character_switch.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*Animation_area.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*AssetsHandling_BooleanToDig_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*AssetsHandling_CoordinateKeyingSuperfluous_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*AssetsHandling_ObjnameObtain_V2.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*Comprehensive_DisplayPackageModelColors_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*Comprehensive_MaskAutomaticSegmentation_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*Comprehensive_MaskInsideAndExtractOutput_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DataAssets_GetMayaMashData_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DataAssets_MayaMashDataAssetCopyReplacement_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DistributedWorkflow_AnimationCharacterSwitch_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DistributedWorkflow_AnimationPerformanceArea_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DistributedWorkflow_DisplaySwitchingControl_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DistributedWorkflow_ExportSplitAdjustmentData_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DistributedWorkflow_ImportSplitAdjustmentData_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DistributedWorkflow_MergeDataExport_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DistributedWorkflow_ObtainObjnameIDGroup_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DistributedWorkflow_PositionSegmentationAdjustment_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*DistributedWorkflow_SubstitutionAdjustmentOutput_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.0\otls\*PlantsSpread_MergeDataExport_V1.0N.hda*
+
+
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*Light topography - Cut out excess.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*Screen_camera_control_switch.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*Type_split_output_by_lens.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*Animation_character_switch.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*Animation_area.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*AssetsHandling_BooleanToDig_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*AssetsHandling_CoordinateKeyingSuperfluous_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*AssetsHandling_ObjnameObtain_V2.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*Comprehensive_DisplayPackageModelColors_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*Comprehensive_MaskAutomaticSegmentation_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*Comprehensive_MaskInsideAndExtractOutput_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DataAssets_GetMayaMashData_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DataAssets_MayaMashDataAssetCopyReplacement_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DistributedWorkflow_AnimationCharacterSwitch_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DistributedWorkflow_AnimationPerformanceArea_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DistributedWorkflow_DisplaySwitchingControl_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DistributedWorkflow_ExportSplitAdjustmentData_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DistributedWorkflow_ImportSplitAdjustmentData_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DistributedWorkflow_MergeDataExport_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DistributedWorkflow_ObtainObjnameIDGroup_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DistributedWorkflow_PositionSegmentationAdjustment_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*DistributedWorkflow_SubstitutionAdjustmentOutput_V1.0N.hda*
+del /q/a/f %userprofile%\Documents\houdini19.5\otls\*PlantsSpread_MergeDataExport_V1.0N.hda*
+
+pause
